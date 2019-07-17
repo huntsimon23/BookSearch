39ebb21d945aab49d2c5c3940fe4ea3246554eff
huntsimon23 <hunt.simon23@gmail.com>
2019-07-17 14:32:06 -0400

    heroku deployment prep

---
+++ client/build/precache-manifest.70d6ca426917fcfe77cbb17e2af90269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22bf9dce8d36983b30ec",
+    "url": "/static/css/main.b19feebe.chunk.css"
+  },
+  {
+    "revision": "22bf9dce8d36983b30ec",
+    "url": "/static/js/main.5e8b25a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5dc4751936c925cbe9f9",
+    "url": "/static/js/2.4d4e686c.chunk.js"
+  },
+  {
+    "revision": "ec16feb8bbd31198132bb997ba8663c0",
+    "url": "/static/media/bookImage.ec16feb8.jpg"
+  },
+  {
+    "revision": "f2fc82b291cf5133814c879c07453877",
+    "url": "/index.html"
+  }
+];

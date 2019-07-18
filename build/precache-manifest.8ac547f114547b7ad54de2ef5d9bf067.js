83cc2bc7719a1be82492f9088bade97e2090b940
huntsimon23 <hunt.simon23@gmail.com>
2019-07-17 21:42:37 -0400

    heroku5

---
+++ build/precache-manifest.8ac547f114547b7ad54de2ef5d9bf067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22bf9dce8d36983b30ec",
+    "url": "/BookSearch/static/css/main.b19feebe.chunk.css"
+  },
+  {
+    "revision": "22bf9dce8d36983b30ec",
+    "url": "/BookSearch/static/js/main.5e8b25a4.chunk.js"
+  },
+  {
+    "revision": "d1126dbf9bec9a6ad6a2",
+    "url": "/BookSearch/static/js/runtime~main.210b1916.js"
+  },
+  {
+    "revision": "5dc4751936c925cbe9f9",
+    "url": "/BookSearch/static/js/2.4d4e686c.chunk.js"
+  },
+  {
+    "revision": "ec16feb8bbd31198132bb997ba8663c0",
+    "url": "/BookSearch/static/media/bookImage.ec16feb8.jpg"
+  },
+  {
+    "revision": "2f0c9173804c70927459f3c04fab9fdf",
+    "url": "/BookSearch/index.html"
+  }
+];
